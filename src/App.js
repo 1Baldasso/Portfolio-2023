c86d6b50f18c6db49000b93f6425c128d2be3c92
Lucas Baldasso <82400557+1Baldasso@users.noreply.github.com>
2023-05-05 22:10:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <title>Lucas Baldasso</title>
           <link rel="canonical" href="https://1baldasso.vercel.app/" />
           <meta property="og:title" content="Lucas Baldasso Portfolio" data-rh="true"/>
-          <meta property="og:description" content="Portfolio Lucas Baldasso feito utilizando React" data-rh="true"/>
+          <meta property="og:description" content="Here is my world, everything I have done on the programming world, feel free to explore!" data-rh="true"/>
           <meta property="og:image" content="https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png" data-rh="true"/>
         </Helmet>
       </HelmetProvider>
